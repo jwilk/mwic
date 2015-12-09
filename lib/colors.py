00caa81b20b42a17e3d2b4a2e8eb1b960dd61cd7
Jakub Wilk <jwilk@jwilk.net>
2015-12-09 15:32:17 +0100

    colors: add docstring.

--- lib/colors.py
+++ lib/colors.py
@@ -18,6 +18,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+'''
+color terminal support
+'''
+
 import io
 import itertools
 import unicodedata

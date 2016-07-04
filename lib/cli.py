jwilk/mwic

commit 718e74194e43da545a78f5cebc5e8e417970b5b0

lib.cli: make --version print also versions of Python and the libraries.

lib/cli.py
@@ -39,9 +39,27 @@ import lib.text
 
 __version__ = '0.7.1'
 
+class VersionAction(argparse._VersionAction):  # pylint: disable=protected-access
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        lines = ['{prog} {0}'.format(__version__, prog=parser.prog)]
+        lines += ['+ Python {0}.{1}.{2}'.format(*sys.version_info)]
+        lines += ['+ PyEnchant {0}'.format(__version__)]
+        try:
+            enchant_version = enchant.get_enchant_version()
+        except AttributeError:
+            pass
+        else:
+            if isinstance(enchant_version, bytes):
+                enchant_version = enchant_version.decode('ASCII', 'replace')
+            lines += ['  + Enchant {0}'.format(enchant_version)]
+        lines += ['+ regex {0}'.format(lib.intdict.re.__version__)]
+        version = ''.join(line + '\n' for line in lines)
+        parser.exit(message=version)
+
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
+    ap.add_argument('--version', action=VersionAction)
     ap.add_argument('files', metavar='FILE', nargs='*', default=['-'])
     ap.add_argument('-l', '--language', metavar='LANG', default='en')
     ap.add_argument('--list-languages', nargs=0, action=list_languages)

commit d31de08ab9dd06bcf478d51da83ddcc52c793202

lib.cli: print version on stdout, not stderr.
Fixes: 718e74194e43da545a78f5cebc5e8e417970b5b0

lib/cli.py
@@ -54,8 +54,9 @@ class VersionAction(argparse._VersionAction):  # pylint: disable=protected-acces
                 enchant_version = enchant_version.decode('ASCII', 'replace')
             lines += ['  + Enchant {0}'.format(enchant_version)]
         lines += ['+ regex {0}'.format(lib.intdict.re.__version__)]
-        version = ''.join(line + '\n' for line in lines)
-        parser.exit(message=version)
+        for line in lines:
+            print(line)
+        parser.exit()
 
 def main():
     ap = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
dbaeb3e0ad475cbf7d3a95d8da144b70779f3d10
Jakub Wilk <jwilk@jwilk.net>
2016-08-28 23:14:10 +0200

    Simplify the no-coverage marker.

--- lib/ns.py
+++ lib/ns.py
@@ -35,7 +35,7 @@ class Namespace(object):
 
 Namespace()  # make pyflakes and coverage.py happy
 
-if sys.version_info >= (3, 3):  # <no-coverage>
+if sys.version_info >= (3, 3):  # no coverage
     Namespace = types.SimpleNamespace  # pylint: disable=no-member
 
 __all__ = ['Namespace']

b9a1adb88f03913de341293ff90338960a1e142d
Jakub Wilk <jwilk@jwilk.net>
2016-12-21 17:23:07 +0100

    lib.cli: simplify the super() call.

--- lib/cli.py
+++ lib/cli.py
@@ -43,7 +43,7 @@ __version__ = '0.7.2'
 class VersionAction(argparse.Action):
 
     def __init__(self, option_strings, dest=argparse.SUPPRESS):
-        super(VersionAction, self).__init__(
+        super().__init__(
             option_strings=option_strings,
             dest=dest,
             nargs=0,

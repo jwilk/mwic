9fb9f474fe7e6719312d2ecf9bb0dda2298adc28
Jakub Wilk <jwilk@jwilk.net>
2018-11-14 17:38:07 +0100

    lib: refactor Python version check.

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,5 +2,4 @@
 mwic's private modules
 '''
 
-# pylint: disable=pointless-statement,not-an-iterable
-lambda: (yield from 0)  # Python >= 3.3 is required
+type(lambda: (yield from []))  # Python >= 3.3 is required
